dd20e9786283a80a98b987b623f42658c864481f
slowdony <435574793@qq.com>
2019-01-28 15:57:34 +0800

    init

---
+++ SDConfigTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SDConfigTest
+//
+//  Created by slowdony on 2019/1/28.
+//  Copyright © 2019年 slowdony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SDConfigTest/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  SDConfigTest
+//
+//  Created by slowdony on 2019/1/28.
+//  Copyright © 2019年 slowdony. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"baseUrl:%@",BaseURL);
+}
+
+
+@end
